fabd31771a572f6974cc495c7cf807fee88363eb
b0nz <elliantosarono.p@gmail.com>
2023-06-21 00:07:52 +0700

    add static

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"embed"
 	"fmt"
+	"io/fs"
 	"net/http"
 )
 
@@ -9,9 +11,16 @@ func HttpHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello World")
 }
 
+//go:embed static
+var resources embed.FS
+
 func main() {
+	directory, _ := fs.Sub(resources, "static")
+	fileServer := http.FileServer(http.FS(directory))
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", HttpHandler)
+	mux.Handle("/hello/", http.StripPrefix("/hello", fileServer))
 
 	server := http.Server{
 		Addr:    ":3000",
